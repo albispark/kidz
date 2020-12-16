c93dc01bca0c98ca64a929eb127d50fffc0c29dd
sofia <sofia.poni@studio.unibo.it>
2020-12-16 16:28:14 +0100

    update sidebar

--- sito/functions.js
+++ sito/functions.js
@@ -1,9 +1,18 @@
-function openNav() {
+sideBarOpen=false;
+
+$(document).ready(function(){
+
+  $('button.openbtn').click(function(e){
     document.getElementById("mySidebar").style.width = "275px";
     document.getElementById("main").style.marginLeft = "275px";
-}
-  
-function closeNav() {
+    document.body.style.overflow = 'hidden';
+    sideBarOpen=true;
+  });
+
+  $('a.closebtn').click(function(e){
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
-  }
+    document.body.style.overflow = 'none';
+    sideBarOpen=false;
+  });
+});
