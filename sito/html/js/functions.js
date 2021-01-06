42232001ab13c75e551063b9da68c6a11e7c220c
sofia <sofia.poni@studio.unibo.it>
2021-01-06 10:51:55 +0100

    modify organization

---
+++ sito/html/js/functions.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+  $("button.openbtn").click(function(e){
+    document.getElementById("mySidebar").style.width = "275px";
+    document.getElementById("main").style.marginLeft = "275px";
+    document.getElementById("mySidebar").style.visibility = "visible";
+    /*var modal = document.getElementById("focus_btn");
+    modal.focus();*/
+    $("body").addClass("fixed_position");
+  });
+
+  $(".btn-close").click(function(e){
+    document.getElementById("mySidebar").style.visibility = "hidden";
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    $("body").removeClass("fixed_position");
+  });
+});
